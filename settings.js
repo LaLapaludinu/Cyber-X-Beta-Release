937f944769c81041feb4b50ccec1c35ff04b21d1
LaLapaludinu <112137445+LaLapaludinu@users.noreply.github.com>
2022-08-30 21:09:34 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ global.APIKeys = {
 
 //AUTO FUNCTIONS 
 
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autobio = false  // auto bio system checking on Developing Leval
@@ -36,15 +36,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.vcardowner = ['94776360685'] //ur owner number
+global.ownername = "ᴋɪɴɢ BLACK KNIGHT" //ur owner name
+global.ytname = "YT: TGM Arcade" // Lapalu Dinujaya
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '9471421243' //ur tag number
+global.owner = ['94776360685']
+global.ownertag = '94776360685' //ur tag number
 global.botname = 'CYBER-X TEST EDITION' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
